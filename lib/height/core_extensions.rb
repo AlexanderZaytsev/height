79dc602411d5a012c85f96de113091b0ed4f63c6
Alexander Zaytsev <alexander.f.zaitsev@gmail.com>
2013-01-11 11:33:30 +0500

    Use only to_height in core extensions

--- lib/height/core_extensions.rb
+++ lib/height/core_extensions.rb
@@ -2,18 +2,10 @@ class Numeric
   def to_height
     Height.new(self)
   end
-
-  def to_centimeters
-    Height::Units::Centimeters.new(self)
-  end
 end
 
 class String
   def to_height
     Height.new(self)
-  end
-
-  def to_centimeters
-    Height::Units::Centimeters.new(self)
   end
 end
--- spec/core_extensions_spec.rb
+++ spec/core_extensions_spec.rb
@@ -6,19 +6,10 @@ describe Numeric do
     it { 191.to_height.should == Height.new(191) }
     it { 191.5.to_height.should == Height.new(191.5) }
   end
-
-  describe '#to_centimeters' do
-    it { 191.to_centimeters.should == Height::Units::Centimeters.new(191) }
-    it { 191.5.to_centimeters.should == Height::Units::Centimeters.new(191.5) }
-  end
 end
 
 describe String do
   describe '#to_height' do
     it { '191 cm'.to_height.should == Height.new(191) }
-  end
-
-  describe '#to_centimeters' do
-    it { '191'.to_centimeters.should == Height::Units::Centimeters.new(191) }
   end
 end
