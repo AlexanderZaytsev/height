59d87ad2c95afbdd6baa0caa350ba5219c28a239
Alexander Zaytsev <alexander.f.zaitsev@gmail.com>
2013-01-11 18:42:58 +0500

    Test for imperial parser

--- spec/parsers/imperial_spec.rb
+++ spec/parsers/imperial_spec.rb
@@ -5,4 +5,5 @@ describe Height::Parsers::Imperial do
   it { Height::Parsers::Imperial.new('6 feet').centimeters.should == Height::Units::Centimeters.new(183) }
   it { Height::Parsers::Imperial.new('6 \'').meters.should == Height::Units::Meters.new(1.83) }
   it { Height::Parsers::Imperial.new('6\' 3"').feet.should == Height::Units::Feet.new(6.25) }
+  it { Height::Parsers::Imperial.new('6ft 3in').feet.should == Height::Units::Feet.new(6.25) }
 end
